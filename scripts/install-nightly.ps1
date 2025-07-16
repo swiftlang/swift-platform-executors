10b9ef970060bc4a43af0a3d0949fcf25708b3c3
Alastair Houghton <ahoughton@apple.com>
2025-07-16 12:10:57 +0100

    Initial attempt to get testing working.
    Added a script to install the nightly build of Swift, and a Github Action
    to use it.

---
+++ scripts/install-nightly.ps1
@@ -0,0 +1,33 @@
+#
+# Download and install the nightly build of Swift.
+#
+
+$webroot="https://download.swift.org/development/windows10"
+
+# Get the name of the latest build
+$latest=Invoke-RestMethod $webroot/latest-build.json
+
+echo "Latest build is $($latest.dir)"
+
+# Fetch the installer
+$installer=Join-Path $Env:Temp install-swift.exe
+echo "Downloading to $installer"
+
+$webClient = New-Object net.webclient
+$webClient.Downloadfile("$webroot/$($latest.dir)/$($latest.download)", "$installer")
+
+# Run the installer
+echo "Installing Swift"
+Start-Process "$installer" -NoNewWindow -Wait -ArgumentList "/q","/jm"
+
+# Delete the installer
+Remove-Item "$installer"
+
+# Update Path
+$env:Path=(
+  [System.Environment]::GetEnvironmentVariable("Path","Machine"),
+  [System.Environment]::GetEnvironmentVariable("Path","User")
+) -match '.' -join ';'
+
+# Display the newly installed Swift version
+swiftc --version
